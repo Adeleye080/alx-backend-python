c95aff45b1b6aae92d0dd381d982e9e7c962d773
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2023-02-07 23:30:08 +0100

    added type annotation.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 """ Async Generator. """
 
 
-async def async_generator() -> Generator:
+async def async_generator() -> Generator[float, None, None]:
     """ Generates 10 random number. """
     for _ in range(10):
         await asyncio.sleep(1)
